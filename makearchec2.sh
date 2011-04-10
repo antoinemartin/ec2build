204b3556f1d96b603357f242a2d71c3bd02ba1b8
root <root@ip-10-226-227-20.(none)>
2011-04-10 19:00:38 +0000

    Cleaned a little bit Added exit on error Removed btrfs and ec2-metadata dependency (unneeded)

--- makearchec2.sh
+++ makearchec2.sh
@@ -16,7 +16,8 @@ fi
 EBSDEVICE=/dev/xvdi
 NEWROOT=/mnt/newroot
 ROOT=${NEWROOT}
-fdisk ${EBSDEVICE} <<EOF
+if [ ! -b ${EBSDEVICE}1 ]; then
+    fdisk ${EBSDEVICE} <<EOF
 n
 p
 
@@ -24,12 +25,14 @@ p
 
 w
 EOF
+fi
+sleep 4
 umount ${NEWROOT}
 
 
-mkfs.ext4 ${EBSDEVICE}1
+mkfs.ext4 ${EBSDEVICE}1 || exit 1
 mkdir ${NEWROOT}
-mount ${EBSDEVICE}1 ${NEWROOT}
+mount ${EBSDEVICE}1 ${NEWROOT} || exit 1
 chmod 755 ${NEWROOT}
 mkdir ${NEWROOT}/boot
 
@@ -37,7 +40,7 @@ PACKS="filesystem pacman sed coreutils ca-certificates groff \
         less which procps logrotate syslog-ng net-tools initscripts psmisc nano vi \
         iputils tar sudo mailx openssh kernel26-ec2 kernel26-ec2-headers \
         wget curl screen bash-completion ca-certificates kernel26-ec2 \
-	kernel26-ec2-headers ec2-metadata btrfs-progs-git zsh ec2arch vim vimpager \
+	kernel26-ec2-headers ec2arch vim vimpager \
 	vim-colorsamplerpack cpio dnsutils base-devel devtools srcpac abs \
 	lesspipe ssmtp iproute2"
 
@@ -59,16 +62,7 @@ EOF
 LC_ALL=C mkarchroot -f -C pacman.conf $ROOT $PACKS
 
 mv $ROOT/etc/pacman.d/mirrorlist $ROOT/etc/pacman.d/mirrorlist.pacorig
-cat <<EOF >$ROOT/etc/pacman.d/mirrorlist
-Server = http://mirrors.kernel.org/archlinux/\$repo/os/\$arch
-Server = ftp://ftp.archlinux.org/\$repo/os/\$arch
-EOF
-
-chmod 666 $ROOT/dev/null
-mknod -m 666 $ROOT/dev/random c 1 8
-mknod -m 666 $ROOT/dev/urandom c 1 9
-mkdir -m 755 $ROOT/dev/pts
-mkdir -m 1777 $ROOT/dev/shm
+cp /etc/pacman.d/mirrorlist $ROOT/etc/pacman.d/mirrorlist
 
 mv $ROOT/etc/rc.conf $ROOT/etc/rc.conf.pacorig
 cat <<EOF >$ROOT/etc/rc.conf
@@ -89,6 +83,7 @@ rm:2345:wait:/etc/rc.multi
 rh:06:wait:/etc/rc.shutdown
 su:S:wait:/sbin/sulogin -p
 ca::ctrlaltdel:/sbin/shutdown -t3 -r now
+0:12345:respawn:/sbin/agetty 38400 hvc0 linux
 EOF
 
 mv $ROOT/etc/hosts.deny $ROOT/etc/hosts.deny.pacorig
@@ -106,7 +101,7 @@ timeout 1
 
 title  Arch Linux
 	root   (hd0,0)
-	kernel /boot/vmlinuz26-ec2 root=/dev/xvda1 ip=dhcp spinlock=tickless ro
+	kernel /boot/vmlinuz26-ec2 root=/dev/xvda1 ip=dhcp console=hvc0 spinlock=tickless ro
 EOF
 
 cd $ROOT/boot
@@ -127,7 +122,7 @@ cp $ROOT/etc/skel/.screenrc $ROOT/root
 mv $ROOT/etc/fstab $ROOT/etc/fstab.pacorig
 
 cat <<EOF >$ROOT/etc/fstab
-$(blkid -c /dev/null -s UUID -o export ${EBSDEVICE}1) / auto    defaults,noauto,relatime 0 0
+$(blkid -c /dev/null -s UUID -o export ${EBSDEVICE}1) / auto    defaults,relatime 0 0
 /dev/xvda3 swap  swap   defaults 0 0
 none      /proc proc    nodev,noexec,nosuid 0 0
 none /dev/pts devpts defaults 0 0
@@ -137,8 +132,8 @@ EOF
 mv $ROOT/etc/makepkg.conf $ROOT/etc/makepkg.conf.pacorig
 cp /etc/makepkg.conf $ROOT/etc/
 
-mkdir $ROOT/opt/{sources,packages,srcpackages}
-chmod 1777 $ROOT/opt/{sources,packages,srcpackages}
+mkdir $ROOT/home/{sources,packages,srcpackages}
+chmod 1777 $ROOT/home/{sources,packages,srcpackages}
 
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> $ROOT/etc/sudoers
 
@@ -147,7 +142,7 @@ echo "nameserver 172.16.0.23" > $ROOT/etc/resolv.conf
 
 touch $ROOT/root/firstboot
 cp -a /root/repo $ROOT/root/
-cp -a /var/cache/pacman/pkg/. $ROOT/var/cache/pacman/pkg/
+#cp -a /var/cache/pacman/pkg/. $ROOT/var/cache/pacman/pkg/
 
 cd $ROOT
 umount ${NEWROOT}
